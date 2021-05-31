7bdddf981d85a0099e5824d6cd3680f1f64c23f4
mahesh yelubolu <yelubolumahesh@yahoo.com>
2021-05-31 15:33:58 +0530

    files uploaded

---
+++ src/com/upgrad/ims/Main.java
@@ -0,0 +1,22 @@
+package com.upgrad.ims;
+public class Main {
+
+    public static void main(String[] args) {
+        Ticket regularTicket = new RegularTicket("12345", "Bengaluru",
+                "Delhi", "DepartureDateTime", "ArrivalDateTime",
+                "6F", 5000, false, null, null, "lunch");
+
+        Ticket touristTicket = new TouristTicket("67890", "India", "Dubai",
+                "DepartureDateTime", "ArrivalDateTIme",
+                "10A", 15000, false, null, null,
+                "Dubai Hotel", new String[]{""});
+
+        printTicketDetails(regularTicket);
+        printTicketDetails(touristTicket);
+    }
+
+    public static void printTicketDetails(Ticket ticket) {
+        System.out.println(ticket.getPnr());
+    }
+
+}
